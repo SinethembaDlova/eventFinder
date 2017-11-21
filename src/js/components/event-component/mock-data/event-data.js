9acb46a637a1e74bb3e7786f8b0ae7dc80a98883
SinethembaDlova <dSinethemba@gmail.com>
2017-11-21 13:32:56 +0200

    updated the mock data

--- src/js/components/event-component/mock-data/event-data.js
+++ src/js/components/event-component/mock-data/event-data.js
@@ -1,10 +1,52 @@
-{
+[
+  {
   "eventID": 1,
-  "eventName" : "Jazz Music Festival",
+  "eventName": "Jazz Music Festival",
   "eventDetails": {
     "eventType": "Music",
     "eventDate": "2017-08-02",
     "eventTime": "18:00 pm",
     "eventSuburb": "Cape Town, Waterfront"
+    }
+  },
+  {
+    "eventID": 2,
+    "eventName": "Art Festival",
+    "eventDetails": {
+      "eventType": "Art",
+      "eventDate": "2017-12-12",
+      "eventTime": "13:00 pm",
+      "eventSuburb": "Cape Town, Khayelitsha"
+      }
+  },
+  {
+    "eventID": 3,
+    "eventName": "Kasi Soccer Tournament",
+    "eventDetails": {
+      "eventType": "Sport",
+      "eventDate": "2017-11-25",
+      "eventTime": "12:00 pm",
+      "eventSuburb": "Cape Town, Green Point"
+      }
+  },
+  {
+    "eventID": 4,
+    "eventName": "Sport Awards",
+    "eventDetails": {
+      "eventType": "Sport",
+      "eventDate": "2017-12-2",
+      "eventTime": "18:00 pm",
+      "eventSuburb": "Cape Town, CBD"
+      }
+  },
+  {
+    "eventID": 5,
+    "eventName": "Ipotsoyi",
+    "eventDetails": {
+      "eventType": "Entainment",
+      "eventDate": "2017-12-1",
+      "eventTime": "12:00 pm",
+      "eventSuburb": "Cape Town, Easteriver"
+      }
   }
-}
+]
