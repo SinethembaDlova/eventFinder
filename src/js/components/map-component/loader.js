cf445dc5a1a0c296827b99f040e854e4627a52ef
Gareth Williams <williams.gareth0@gmail.com>
2017-11-21 11:49:09 +0200

    modified map component naming and created html view

--- src/js/components/map-component/loader.js
+++ src/js/components/map-component/loader.js
@@ -1,6 +1,6 @@
 define(['ojs/ojcore', 'text!./view.html', './viewModel', 'text!./component.json', 'css!./styles', 'ojs/ojcomposite'],
   function(oj, view, viewModel, metadata) {
-    oj.Composite.register('weather-component', {
+    oj.Composite.register('map-component', {
       view: {inline: view},
       viewModel: {inline: viewModel},
       metadata: {inline: JSON.parse(metadata)}
