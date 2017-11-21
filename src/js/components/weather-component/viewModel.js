f7b37ed2c341f3ffa0dc66c3d3bd66d44a5b66fc
Gareth Williams <williams.gareth0@gmail.com>
2017-11-21 11:53:43 +0200

    added json object for weather component, and updated the viewModel for the weather component, and the loader for requiring the files as well

---
+++ src/js/components/weather-component/viewModel.js
@@ -0,0 +1,11 @@
+define(
+    ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $, ace, monokai) {
+    'use strict';
+
+    function weatherViewModel(context) {
+        var self = this;
+
+    };
+
+    return weatherViewModel;
+});
