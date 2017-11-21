f4449d90fcee391925d8cdb02fbc777092fc4626
SinethembaDlova <dSinethemba@gmail.com>
2017-11-21 12:36:18 +0200

    accessed the mock-data using ajax call

--- src/js/components/event-component/viewModel.js
+++ src/js/components/event-component/viewModel.js
@@ -1,11 +1,16 @@
 define(
-    ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $, ace, monokai) {
-    'use strict';
+    ['ojs/ojcore', 'knockout', 'jquery'],
+    function(oj, ko, $, ace, monokai) {
+      'use strict';
 
-    function eventViewModel(context) {
+      function eventViewModel(context) {
         var self = this;
+        var url = './js/components/event-component/mock-data/event-data.js';
 
-    };
+        $.getJSON(url).done(function(data) {
+            console.log(data);
+          })
+        };
 
-    return eventViewModel;
-});
+        return eventViewModel;
+      });
