93098a8a304e6a47377dc45c28e48ebc7c56cadc
Gareth Williams <williams.gareth0@gmail.com>
2017-11-21 11:50:00 +0200

    created binding view model for map component

---
+++ src/js/components/map-component/viewModel.js
@@ -0,0 +1,11 @@
+define(
+    ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $, ace, monokai) {
+    'use strict';
+
+    function mapViewModel(context) {
+        var self = this;
+
+    };
+
+    return mapViewModel;
+});
