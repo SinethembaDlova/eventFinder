2ec0ef9facb834dc7fb7520ef95bc3e79fead6af
Gareth Williams <williams.gareth0@gmail.com>
2017-11-22 09:49:04 +0200

    working on the filter dropboxes, and moved the weather component into the event component

--- src/js/components/event-component/viewModel.js
+++ src/js/components/event-component/viewModel.js
@@ -9,20 +9,26 @@ define(
 
         self.allEvents = ko.observableArray([]);
 
+        self.allTypes = ko.observableArray(['All']);
+
+        self.SearchType = ko.observable("");
+
+        self.chosenType = ko.observableArray(['All']);
+
         self.eventDetails = function(currentEvent) {
             console.log(currentEvent)
         }
 
 
-
         $.getJSON(url).done(function(data) {
-            // console.log(data);
-
+            var typeMap = [];
             data.forEach(function(events){
-              // console.log(events);
               self.allEvents.push(events);
+              if (typeMap[events.eventDetails.eventType] === undefined) {
+                typeMap[events.eventDetails.eventType] = 0;
+                self.allTypes.push(events.eventDetails.eventType);
+              };
             });
-
           });
         };
 
