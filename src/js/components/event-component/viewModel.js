baa342a4d5d10338d6accf7414045e9b88bf60f7
Gareth Williams <williams.gareth0@gmail.com>
2017-11-22 12:07:26 +0200

    updated filter functionality

--- src/js/components/event-component/viewModel.js
+++ src/js/components/event-component/viewModel.js
@@ -7,30 +7,59 @@ define(
         var self = this;
         var url = './js/components/event-component/mock-data/event-data.js';
 
+        // event data
         self.allEvents = ko.observableArray([]);
-
-        self.allTypes = ko.observableArray(['All']);
-
-        self.SearchType = ko.observable("");
-
-        self.chosenType = ko.observableArray(['All']);
+        self.masterData = ko.observableArray();
 
         self.eventDetails = function(currentEvent) {
-            console.log(currentEvent)
+          console.log(currentEvent)
+        }
+
+        $.getJSON(url).done(function(data) {
+          self.masterData.push(data);
+
+          var typeMap = [];
+          for (var i = 0; i < data.length; i++) {
+            self.allEvents.push(data[i]);
+
+            if (typeMap[data[i].eventDetails.eventType] === undefined) {
+              typeMap[data[i].eventDetails.eventType] = 0;
+              self.allTypes.push(data[i].eventDetails.eventType);
+            };
+          }
+        });
+
+        // filter by type
+        self.allTypes = ko.observableArray(['All']);
+        self.chosenType = ko.observableArray(['All']);
+        self.filter = ko.observableArray();
+
+        self.onclick = function(typeVal) {
+          let data = self.allEvents();
+
+          for (var i = 0;  i < data.length; i++) {
+            if (data[i].eventDetails.eventType === typeVal) {
+              self.filter.push(data[i]);
+            }
+            // console.log(data[i].eventDetails)
+          }
+
+          if (self.filter().length === 0) {
+            // self.allEvents([])
+            // self.allEvents.push(self.masterData());
+          } else {
+            let filteredData = self.filter()
+            console.log(self.filter());
+            self.allEvents([])
+
+            for (var x =0; x < filteredData.length; x++) {
+              self.allEvents.push(filteredData[x]);
+            }
+          }
+
         }
 
 
-        $.getJSON(url).done(function(data) {
-            var typeMap = [];
-            for (var i = 0; i < data.length; i++) {
-              self.allEvents.push(data[i]);
-
-              if (typeMap[data[i].eventDetails.eventType] === undefined) {
-                typeMap[data[i].eventDetails.eventType] = 0;
-                self.allTypes.push(data[i].eventDetails.eventType);
-              };
-            }
-          });
         };
 
 
