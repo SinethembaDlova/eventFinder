38c2e5f5179642cc4e0b038bb872b01916d7f7c1
Siyasanga <siyasanga@projectcodex.co>
2017-11-22 12:29:21 +0200

    Use data-binding to display weather icon

--- src/js/components/weather-component/viewModel.js
+++ src/js/components/weather-component/viewModel.js
@@ -2,10 +2,23 @@ define(
     ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $, ace, monokai) {
     'use strict';
 
-    function weatherViewModel(context) {
+    function WeatherViewModel(context) {
         var self = this;
-        
+        var capeId;
+        self.iconUrl = ko.observable()
+
+        $.getJSON("js/components/weather-component/cape.json", function(results) {
+          capeId = results[0].woeid;
+          // console.log(capeId);
+        });
+        var weatherState;
+        $.getJSON("js/components/weather-component/CapeWeatherNext5days.json", function(results) {
+          // var weatherState = results.consolidated_weather[0].weather_state_abbr;
+          var weatherState = "https://www.metaweather.com/static/img/weather/hr.svg";
+          var url = "url('" + weatherState + "')";
+          self.iconUrl(url);
+        })
     };
 
-    return weatherViewModel;
+    return WeatherViewModel;
 });
